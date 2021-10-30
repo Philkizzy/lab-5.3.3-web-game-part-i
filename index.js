e7a0ec8467def1f73665cdbc33861c4f4347251b
PhilGroup <64746978+PhilGroup@users.noreply.github.com>
2021-10-30 07:58:07 -0400

    after removing last commit

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+
+
+function newImage(srcImage, leftValue, bottomValue){
+let path = document.createElement('img')
+path.src = srcImage
+path.style.position = 'fixed'
+path.style.left = leftValue
+path.style.bottom = bottomValue
+document.body.append(path)
+
+}
+
+function newItem(srcImage, leftValue, bottomValue){
+let sword = document.createElement('img')
+sword.src = srcImage
+sword.position = 'fixed'
+sword.left = leftValue
+sword.bottom = bottomValue
+document.body.append(sword)
+
+sword.addEventListener('dblclick', function(){
+    sword.remove()
+})
+
+}
+    // invoke newImage function
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
+    // Invoke newItem function
+    newItem('assets/sheild.png','165px','185px')
+    newItem('assets/staff.png', '600px', '100px')
+
+    
+
+   
+
+   
+    
+  
+
